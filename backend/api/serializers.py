011c04ec65d5437c0748fbf8b4fa27cd06c7e622
Vlada Rubina <vlada.v.rubina@gmail.com>
2023-09-16 23:24:22 +0300

    fixed 1st review

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -69,7 +69,7 @@ class UserWithRecipesSerializer(UserSerializer):
 
         recipes_limit = self.context.get('request').GET.get(
             'recipes_limit', settings.RECIPES_LIMIT_DEFAULT
-            )
+        )
         author_recipes = author_recipes[:int(recipes_limit)]
 
         serializer = RecipeListSerializer(
@@ -110,8 +110,9 @@ class RecipeIngredientsSerializer(serializers.ModelSerializer):
     id = serializers.StringRelatedField(source='product.id')
     name = serializers.CharField(source='product.name', read_only=True)
     measurement_unit = serializers.CharField(
-        source='product.measurement_unit', read_only=True
-        )
+    source='product.measurement_unit',
+    read_only=True
+    )
 
     class Meta:
         model = RecipeIngredient
@@ -182,9 +183,9 @@ class RecipeSerializer(serializers.ModelSerializer):
 
     def get_is_add(self, obj, model):
         user = self.context['request'].user
-        return not user.is_anonymous and model.objects.filter(
-            user=user, recipe=obj
-            ).exists()
+        return not user.is_anonymous and \
+            model.objects.filter(user=user, recipe=obj).exists()
+
 
 
 class RecipeCreateUpdateSerializer(serializers.ModelSerializer):
